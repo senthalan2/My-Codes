15c1ba8d6aefc0b47af12060ac0a24fe0d3d82f4
senthalan2 <43330632+senthalan2@users.noreply.github.com>
2018-09-18 13:50:27 +0530

    Create Positive_Negative.c

---
+++ Positive_Negative.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+  int input;
+  scanf("%d",&input);
+  if(input>0)
+  {
+    printf("Positive");
+  }
+  else if(input==0)
+  {
+    printf("Zero");
+  }
+  else
+  {
+    printf("Negative");
+  }
+}
+  
