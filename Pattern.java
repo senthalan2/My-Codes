eae129ec136150ddea347b04f26acd5d8f3a9d75
senthalan2 <43330632+senthalan2@users.noreply.github.com>
2018-10-11 18:29:23 +0530

    Create Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,25 @@
+class Demo
+{
+public static void main(String arg[])
+{
+int i=1;
+for(i=1;i<10;i++)
+{
+	if(i==1)
+	{
+	System.out.print("  ");
+	}
+	else if(i==2)
+	{
+	System.out.println();
+	System.out.print(" ");
+	}
+	else if(i==5)
+	{
+	System.out.println();
+	}
+	System.out.print(i);
+	
+}
+}
+}
